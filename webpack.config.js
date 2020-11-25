bfc41872c4dc65b458a61fb3a59296c7604f4fb0
sat.dzmitry.prokharau <sat.dzmitry.prokharau@gmail.com>
2020-11-25 14:46:59 +0300

    Was added css-loader & style-loader

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
